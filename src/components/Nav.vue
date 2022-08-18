c94f310a47e838275acf5e75772f3dec5c657eb5
Laura López Alonso <laura.lopez.alo@gmail.com>
2022-08-18 16:17:30 +0200

    kick off

---
+++ src/components/Nav.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>Nav Component</div>
+  <h3>Esto es el navbar</h3>
+</template>
+
+<script setup>
+//constant to save a variable that will hold the use router method
+
+// constant to save a variable that will get the user from store with a computed function imported from vue
+
+// constant that calls user email from the useUSerStore
+
+// constant that saves the user email and cleans out the @client from the user
+
+// async function that calls the signOut method from the useUserStore and pushes the user back to the Auth view.
+</script>
+
+<style></style>
