568773a28884a96b99919a790cbf49ea414547c1
Laura López Alonso <laura.lopez.alo@gmail.com>
2022-08-22 17:14:06 +0200

    Log In, Sign Up, Log Out

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -1,40 +1,80 @@
 <template>
+  <!-- <div
+    class="bg-gradient-to-r from-violet-500 to-fuchsia-500  absolute top-0 right-0 bottom-0 left-0"
+  > -->
   <div>
-    <h1>Log In to TaskApp</h1>
+    <div class="flex justify-center items-baseline h-full pt-4 sm:p-16">
+      <div
+        class="w-[28rem] relative py-16 px-14 bg-white shadow-md border-none rounded-3xl"
+      >
+        <div class="w-full mb-6 flex justify-center items-center">
+          <div>
+            <h1 class="text-6xl text-gray-800 mb-5 font-bold text-center">
+              TaskApp
+            </h1>
+            <h3
+              class="text-lg font-medium text-gray-900 py-3 tracking-wide text-center"
+            >
+              Organize your life and your work <br />
+              with a single and simple tool.
+            </h3>
+          </div>
+        </div>
 
-    <h2>Start Organizing your tasks todays!</h2>
-  </div>
-
-  <div class="main-container">
-    <form>
-      <div class="box-container">
-        <div class="form-fields">
-          <label for="email">Email</label>
-          <input
-            id="email"
-            name="email"
-            type="text"
-            placeholder="your@email.com"
-          />
+        <div>
+          <form @submit.prevent="signIn" class="space-y-6">
+            <div class="w-full flex flex-col">
+              <label
+                for="email"
+                class="text-sm font-medium text-gray-900 block mb-1"
+                >Email</label
+              >
+              <input
+                v-model="email"
+                name="email"
+                type="text"
+                placeholder="your@email.com"
+                required
+                class="input basis-3/4 mb-3 bg-transparent border-0 border-b-2 rounded-none p-3 focus:outline-none border-b-slate-900 text-gray-900 placeholder:ttext-gray-400 shadow-md"
+              />
+            </div>
+            <div class="w-full flex flex-col">
+              <label
+                for="password"
+                class="text-sm font-medium text-gray-900 block mb-1"
+                >Password</label
+              >
+              <input
+                v-model="password"
+                name="password"
+                type="password"
+                placeholder="*******"
+                class="input basis-3/4 mb-6 bg-transparent border-0 border-b-2 rounded-none p-3 focus:outline-none border-b-slate-900 text-gray-900 placeholder:ttext-gray-400 shadow-md"
+              />
+            </div>
+            <div>
+              <button
+                class="w-full text-white bg-violet-700 hover:bg-violet-800 focus:outline-none focus:ring-2 focus:ring-violet-600 focus:shadow-lg font-medium rounded-lg text-sm px-5 py-3 text-center mb-2"
+                name="commit"
+                type="submit"
+              >
+                Log In
+              </button>
+            </div>
+            <div>
+              <p class="text-sm font-medium text-gray-900 block mb-1">
+                Not registered?
+                <PersonalRouter
+                  :route="route"
+                  :buttonText="buttonText"
+                  class="text-violet-700 hover:underline"
+                />
+              </p>
+            </div>
+          </form>
         </div>
-        <div class="form-fields">
-          <label for="password">Password</label>
-          <input
-            id="password"
-            name="password"
-            type="text"
-            placeholder="*******"
-          />
-        </div>
-        <div class="form-fields">
-          <button class="signIn" name="commit" type="submit">Sign In</button>
-        </div>
-        <p>
-          Don't have an account?
-          <PersonalRouter :route="route" :buttonText="buttonText" />
-        </p>
       </div>
-    </form>
+    </div>
   </div>
 </template>
 
@@ -48,7 +88,9 @@ import { storeToRefs } from "pinia";
 
 // Route Variables
 const route = "/auth/sign-up";
-const buttonText = "Sign Up";
+const buttonText = "Create an account";
+
+const user = useUserStore();
 
 // Input Fields
 const email = ref("");
@@ -69,14 +111,10 @@ const redirect = useRouter();
 // Arrow function to Signin user to supaBase
 const signIn = async () => {
   try {
-    // calls the user store and send the users info to backend to logIn
-    await useUserStore().signIn(email.value, password.value);
-    // redirects user to the homeView
+    await user.signIn(email.value, password.value);
     redirect.push({ path: "/" });
   } catch (error) {
-    // displays error message
     errorMsg.value = `Error: ${error.message}`;
-    // hides error message
     setTimeout(() => {
       errorMsg.value = null;
     }, 5000);
@@ -84,28 +122,4 @@ const signIn = async () => {
 };
 </script>
 
-<style>
-.wu-text {
-  color: black;
-}
-
-.form {
-  display: flex;
-  flex-direction: column;
-  margin: 1rem 0;
-}
-.input {
-  color: black;
-  margin-bottom: 1rem;
-}
-.button {
-  background-color: #4caf50; /* Green */
-  border: none;
-  color: white;
-  padding: 10px 10px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-}
-</style>
+<style></style>
--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,45 +1,93 @@
 <template>
-   <div>
-    <h1>Log In to TaskApp</h1>
-
-    <h2>Start Organizing your tasks todays!</h2>
-  </div>
-  <form>
-      <div class="box-container">
-        <div class="form-fields">
-          <label for="email">Email</label>
-          <input
-            id="email"
-            name="email"
-            type="text"
-            placeholder="your@email.com"
-          />
+  <!-- <div
+    class="bg-gradient-to-r from-violet-500 to-fuchsia-500  absolute top-0 right-0 bottom-0 left-0"
+  > -->
+  <div>
+    <div class="flex justify-center items-baseline h-full pt-4 sm:p-16">
+      <div
+        class="w-[28rem] relative py-16 px-14 bg-white shadow-md border-none rounded-3xl"
+      >
+        <div class="w-full mb-6 flex justify-center items-center">
+          <div>
+            <h1 class="text-6xl text-gray-800 mb-5 font-bold text-center">
+              TaskApp
+            </h1>
+            <h3
+              class="text-lg font-medium text-gray-900 py-3 tracking-wide text-center"
+            >
+              Sign up for free and say hello<br />to your new organized life.
+            </h3>
+          </div>
         </div>
-        <div class="form-fields">
-          <label for="password">Password</label>
-          <input
-            id="password"
-            name="password"
-            type="password"
-            placeholder="*******"
-           
-          />
+        <div v-if="errorMsg" class="mb-6">
+          <p class="text-lg font-medium text-red-500">{{errorMsg}}</p>
         </div>
-        <div class="form-fields">
-          <label for="repeat-password">Repeat password</label>
-          <input
-            id="repeat-password"
-            name="repeat-password"
-            type="password"
-            placeholder="*******"
-          />
+        <div>
+          <form @submit.prevent="signUp" class="space-y-6">
+            <div class="w-full flex flex-col">
+              <label
+                for="email"
+                class="text-sm font-medium text-gray-900 block mb-1"
+                >Email</label
+              >
+              <input
+                v-model="email"
+                type="text"
+                placeholder="your@email.com"
+                required
+                class="input basis-3/4 mb-3 bg-transparent border-0 border-b-2 rounded-none p-3 focus:outline-none border-b-slate-900 text-gray-900 placeholder:ttext-gray-400 shadow-md"
+              />
+            </div>
+            <div class="w-full flex flex-col">
+              <label
+                for="password"
+                class="text-sm font-medium text-gray-900 block mb-1"
+                >Password</label
+              >
+              <input
+                v-model="password"
+                type="password"
+                placeholder="*******"
+                class="input basis-3/4 mb-6 bg-transparent border-0 border-b-2 rounded-none p-3 focus:outline-none border-b-slate-900 text-gray-900 placeholder:ttext-gray-400 shadow-md"
+              />
+            </div>
+            <div class="w-full flex flex-col">
+              <label
+                for="confirmPassword"
+                class="text-sm font-medium text-gray-900 block mb-1"
+                >Confirm password</label
+              >
+              <input
+                v-model="confirmPassword"
+                type="password"
+                placeholder="*******"
+                class="input basis-3/4 mb-6 bg-transparent border-0 border-b-2 rounded-none p-3 focus:outline-none border-b-slate-900 text-gray-900 placeholder:ttext-gray-400 shadow-md"
+              />
+            </div>
+            <div>
+              <button
+                class="w-full text-white bg-violet-700 hover:bg-violet-800 focus:outline-none focus:ring-2 focus:ring-violet-600 focus:shadow-lg font-medium rounded-lg text-sm px-5 py-3 text-center mb-2"
+                name="commit"
+                type="submit"
+              >
+                Let's begin!
+              </button>
+            </div>
+            <div>
+              <p class="text-sm font-medium text-gray-900 block mb-1">
+                Have an account?
+                <PersonalRouter
+                  :route="route"
+                  :buttonText="buttonText"
+                  class="text-violet-700 hover:underline"
+                />
+              </p>
+            </div>
+          </form>
         </div>
-        <div class="form-fields">
-          <button class="signIn" name="commit" type="submit">Sign In</button>
-        </div>
-        
       </div>
-    </form>
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -52,10 +100,14 @@ import { storeToRefs } from "pinia";
 
 // Route Variables
 const route = "/auth/login";
+const buttonText = "Log In";
+
+//const user = useUserStore();
 
 // Input Fields
 const email = ref("");
 const password = ref("");
+const confirmPassword = ref("");
 
 // Error Message
 const errorMsg = ref("");
@@ -70,19 +122,25 @@ const hidePassword = ref(true);
 const redirect = useRouter();
 
 // Arrow function to Signin user to supaBase
-const SignUp = async () => {
-  try {
-    // calls the user store and send the users info to backend to logIn
-    await useUserStore().signUp(email.value, password.value);
-    // redirects user to the homeView
-    redirect.push({ path: "/" });
-  } catch (error) {
-    // displays error message
-    errorMsg.value = `Error: ${error.message}`;
-    // hides error message
-    setTimeout(() => {
-      errorMsg.value = null;
-    }, 5000);
+const signUp = async () => {
+  if (password.value === confirmPassword.value) {
+    try {
+      await user.signUp(email.value, password.value);
+     // if (error) throw error;
+      redirect.push({ path: "/" });
+    } catch (error) {
+      errorMsg.value = `Error: ${error.message}`;
+      setTimeout(() => {
+        errorMsg.value = null;
+      }, 5000);
+    }
+    return;
+  }
+  else {
+  errorMsg.value = "Passwords do not match";
+  setTimeout(() => {
+    errorMsg.value = null;
+  }, 5000);
   }
 };
 </script>
